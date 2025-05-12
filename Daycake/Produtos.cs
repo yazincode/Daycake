f8e39df798208438aa62881c4b5431846914c95b
Yasmin Dantas <yasmindantasbusiness@gmail.com>
2025-05-12 19:23:51 -0300

    Arrumado o database.

--- Daycake/Produtos.cs
+++ Daycake/Produtos.cs
@@ -16,7 +16,7 @@ namespace Daycake
     public partial class Produtos : Form
     {
         MySqlConnection Conexao;
-        private string data_source = "datasource=localhost;username=root;password=1007;database=daycake";
+        private string data_source = "datasource=localhost;username=root;password=;database=daycake";
         public int? id_produto_selecionado = null;
 
         public Produtos()
